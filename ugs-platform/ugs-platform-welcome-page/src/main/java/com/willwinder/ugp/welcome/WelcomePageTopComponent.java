51f670e52e57735f7ce7958728625be08db3f077
FLK-Corentin <corentin@fablab.com>
2026-09-09 13:53:08 +0200

    Update WelcomePageTopComponent.java

--- ugs-platform/ugs-platform-welcome-page/src/main/java/com/willwinder/ugp/welcome/WelcomePageTopComponent.java
+++ ugs-platform/ugs-platform-welcome-page/src/main/java/com/willwinder/ugp/welcome/WelcomePageTopComponent.java
@@ -49,8 +49,8 @@ public final class WelcomePageTopComponent extends TopComponent {
     protected final static String Category = "Help";
     protected final static String ActionId = "com.willwinder.ugp.welcome.WelcomePageTopComponent";
     protected final static String Path = "Menu/Help";
-    protected final static String Title = "Welcome Page";
-    protected final static String Tooltip = "Welcome Page";
+    protected final static String Title = "Page d'accueil";
+    protected final static String Tooltip = "Page d'accueil";
     // Flag to prevent opening the start page once, allowing it to be opened manually.
     private static boolean firstTimeOpen = true;
 
